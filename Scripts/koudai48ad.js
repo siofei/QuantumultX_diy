cbcd22e40fda58f28a3ae512e2ebad5831e38dd9
siofei <liu.xingfei@outlook.com>
2022-05-08 01:42:13 +0800

    口袋48广告

--- Scripts/koudai48ad.js
+++ Scripts/koudai48ad.js
@@ -1,6 +1,4 @@
 
-// 感谢 blackmatrix7 提供的思路，但原js过于庞大，所以将此单独拿出来解析
-// 原版地址 https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/bilibili/bilibili_plus.js
 
 let body = $response.body
 body = JSON.parse(body)
