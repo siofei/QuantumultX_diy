8ad7bc8ea5e32fe89bfd09e7ad3263e6cfe08a54
siofei <liu.xingfei@outlook.com>
2022-05-10 00:08:04 +0800

    add

--- Scripts/koudai48ad.js
+++ Scripts/koudai48ad.js
@@ -5,6 +5,7 @@ body = JSON.parse(body)
 if (body.hasOwnProperty('content')) {
     if (body['content'].hasOwnProperty('adType')){
         body['content']['adType'] = 0
+        console.log("去开屏广告")
     }
     // if (body['content'].hasOwnProperty('show')){
     //     body['content']['show'] = false
